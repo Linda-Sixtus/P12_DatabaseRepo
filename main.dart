12c593fb3c239ad57d5d06a2bb84d3c209a3febc
Linda Sixtus <linda.sixtus@gmx.de>
2025-06-30 16:09:20 +0200

    P12_DatabaseRepo_3

--- main.dart
+++ main.dart
@@ -1,31 +1,58 @@
-
 //_____----_______----_______________Aufgabe 2
 
 import 'dart:ffi';
 import "dart:io";
 
 abstract class DatabaseRepository {
-  List<SunimoMessage> Read();
-  bool Create(SunimoMessage message);
-  bool Update(SunimoMessage message);
-  bool Delete(SunimoMessage message);
+  List<Message?> getMessages();
+  bool sendMessage(Message message);
+  bool updateMessage(String id, String newMessage);
+  bool deleteMessage(String id);
 }
 
-class SunimoMessage {
+class Message {
   String? message;
-  int? messageId;
+  String? messageId;
 }
 
 //_____----_______----_______________Aufgabe 3
 
-
-class MockDatabaseRepository implements DatabaseRepository {
-  List<String> messages = [];
+abstract class MockDatabaseRepository implements DatabaseRepository {
+  List<Message?> messages = [];
 
   @override
-  void sendMessage(String m) {
+  bool sendMessage(Message m) {
     messages.add(m);
+
+    return true; // Eventuell Fehler abfragen, dann false senden
   }
+
   @override
-  List<String> getMessages() {
-    return messageList;
+  List<Message?> getMessages() {
+    return messages;
+  }
+
+  @override
+  bool updateMessage(String id, String newMessage) {
+    Message? message = messages.firstWhere(
+      (message) => message?.messageId == id,
+    );
+    if (message == null) {
+      return false;
+    }
+    message.message = newMessage;
+    return true;
+  }
+
+  @override
+  bool deleteMessage(String id) {
+    Message? message = messages.firstWhere(
+      (message) => message?.messageId == id,
+    );
+    if (message == null) {
+      return false;
+    }
+    messages.remove(message);
+    return true;
+  }
+}
